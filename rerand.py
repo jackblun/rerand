be5b86bf9a52c40317f14e6e4a580f052e3a15a0
Jack Blundell <jackblun@gmail.com>
2022-06-07 16:51:17 +0200

    Add rerandomisation class

--- rerand.py
+++ rerand.py
@@ -1,26 +1,52 @@
 import numpy as np
 
 
-def distance(x0, x1, metric="Euclidean"):
-    """
-    Function that calculates distance between two NxK matrices,
-    where N is the number of observations and K is the
-    number of variables
+class Randomisation:
+    def __init__(self, covariates, distance_metric, tol, max_reps):
+        self.covariates = covariates
+        self.distance_metric = distance_metric
+        self.tol = tol
+        self.max_reps = max_reps
+        self.n = len(covariates)
 
-    Params:
-    - x0: NxK matrix of covariates from control group
-    - x1: NXK matrix of covaraites from treatment group
-    - metric: Distance metric used. Supported options are Euclidean.
+    def randomise(self):
 
-    Returns:
-    - distance: Distance between two matrices
-    """
+        for i in range(self.max_reps):
+            t_p = np.random.uniform(low=0, high=1, size=self.n)
+            t = t_p > 0.5
 
-    x0_bar = np.mean(x0, axis=0)
-    x1_bar = np.mean(x1, axis=0)
+            x0 = self.covariates[~t]
+            x1 = self.covariates[t]
 
-    if metric == "Euclidean":
-        sq_devs = np.power(x0_bar - x1_bar, 2)
-        distance = np.sqrt(np.sum(sq_devs))
+            dist = self.distance(x0, x1)
+            if dist < self.tol:
+                print(
+                    str(i) + " randomisations needed to achieve balance"
+                    "with tolerance " + str(self.tol)
+                )
+                return t
+        print("Did not achieve balance for tolerance " + str(self.tol))
 
-    return distance
+    def distance(self, x0, x1, metric="Euclidean"):
+        """
+        Method that calculates distance between two NxK matrices,
+        where N is the number of observations and K is the
+        number of variables
+
+        Params:
+        - x0: NxK matrix of covariates from control group
+        - x1: NXK matrix of covaraites from treatment group
+        - metric: Distance metric used. Supported options are Euclidean.
+
+        Returns:
+        - distance: Distance between two matrices
+        """
+
+        x0_bar = np.mean(x0, axis=0)
+        x1_bar = np.mean(x1, axis=0)
+
+        if metric == "Euclidean":
+            sq_devs = np.power(x0_bar - x1_bar, 2)
+            distance = np.sqrt(np.sum(sq_devs))
+
+        return distance
